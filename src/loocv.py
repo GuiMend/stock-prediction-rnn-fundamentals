3b919621a0372374efa4fc4df6a737498cc51464
Guilherme Mendonca <guilhermemendonca@Guilhermes-MacBook-Pro.local>
2019-10-28 00:14:35 -0300

    feat: implement loocv

--- src/loocv.py
+++ src/loocv.py
@@ -1,28 +1,23 @@
+import numpy as np
 import tcc_utils
 import tratamento_dados_empresa
+from sklearn.preprocessing import StandardScaler
 from keras.initializers import glorot_normal as gl_normal
 
 x_csv_path = '../data/grendene-indicadores-financeiros-raw.csv'
 y_csv_path = '../data/grendene-cotacao-raw.csv'
 
-X = tratamento_dados_empresa.treat_economatica_indicadores_financeiros(x_csv_path)
-
-# Spliting test / train and Feature Scaling
-x_train, x_test, y_train, y_test, x_scaler, y_scaler = tratamento_dados_empresa. \
-    get_scaled_splits_and_scaler(x_csv_path, y_csv_path, 0.2, 0)
-
-number_hidden_layer = round(x_train.shape[1]*1.5)
-
-
 x, y = tratamento_dados_empresa.get_x_y(x_csv_path, y_csv_path)
+x_int_index = x.reset_index()
 
-for i in range(x.shape[1]):
-    train = x.drop(i)
-    test = x[i]
+number_hidden_layer = round(x.shape[1]*1.5)
 
-
-
-
+# Feature Scaling X
+x_scaler = StandardScaler()
+x = x_scaler.fit_transform(x)
+# Feature Scaling y
+y_scaler = StandardScaler()
+y = y_scaler.fit_transform(y)
 
 # Keras Initializer with seed
 glorot_normal = gl_normal(seed=0)
@@ -30,7 +25,20 @@ glorot_normal = gl_normal(seed=0)
 layers = [number_hidden_layer, number_hidden_layer]
 activations = ['relu', 'relu']
 # Create model
-deep_model = tcc_utils.deep_model(x_train, glorot_normal, layers, activations)
+deep_model = tcc_utils.deep_model(x, glorot_normal, layers, activations)
+
+
+results = []
+
+for i in range(x.shape[0]):
+    x_train = np.delete(x, i, 0)
+    y_train = np.delete(y, i, 0)
+    x_test = x[i:i+1]
+    y_test = y[i:i+1]
+    deep_model.train_on_batch(x_train, y_train)
+    predictions_real = y_scaler.inverse_transform(deep_model.predict(x_test))
+    y_real = y_scaler.inverse_transform(y_test)
+    print(f'R$ {predictions_real[0,0]} - R$ {y_real[0,0]} - error: {tcc_utils.calculate_diff_percent(predictions_real[0,0], y_real[0,0])}')
 
 # Evaluating model
 tcc_utils.evaluate_model(deep_model, deep_history, x_train, y_train,
